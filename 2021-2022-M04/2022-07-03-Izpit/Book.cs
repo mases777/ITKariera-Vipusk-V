2dc293a6e7150b0b39e2c87cbaaff227ac082039
mases777 <mases@abv.bg>
2022-07-25 20:34:35 +0300

    M04-Edit2

---
+++ 2021-2022-M04/2022-07-03-Izpit/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Program
+{
+    public class Book
+    {
+        private string title;
+        private double rating;
+
+        public Book(string title, double rating)
+        {
+            Title = title;
+            Rating = rating;
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                title = value;
+            }
+        }
+
+        public double Rating
+        {
+            get
+            {
+                return rating;
+            }
+
+            set
+            {
+                rating = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Book {title} is with {rating} rating.";
+        }
+    }
+}
