81a432b3f7923f671de85b5542b46dc0b1537771
mases777 <mases@abv.bg>
2022-09-10 09:38:42 +0300

    M05

---
+++ 2022-2023-M05/2022-M05-OOP/BankAcoountTests/BankAccountTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zadacha01;
+
+namespace BankAcoountTests
+{
+    public class BankAccountTests
+    {
+        [TestFixture]
+        public class BankAcountTests
+        {
+            [Test]
+            public void AcountInitializeWithPositiveValue()
+            {
+                BankAccount acount = new BankAccount(2000m);
+                Assert.AreEqual(2000m, acount.Balance);
+            }
+        }
+    }
+}
---
+++ 2022-2023-M05/2022-M05-OOP/Zadacha01/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha01
+{
+    public class BankAccount
+    {
+        private decimal balance;
+
+        public decimal Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+        public BankAccount(decimal amount = 0)
+        {
+            this.Balance = amount;
+        }
+
+        public void Deposit(decimal cash)
+        {
+            this.Balance += cash;
+        }
+    }
+}
