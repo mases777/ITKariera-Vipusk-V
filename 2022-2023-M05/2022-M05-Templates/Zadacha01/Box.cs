743d20857f2eaab2f47b07716449345986a48af8
mases777 <mases@abv.bg>
2022-09-17 09:20:58 +0300

    M05-Zadacha01

---
+++ 2022-2023-M05/2022-M05-Templates/Zadacha01/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha01
+{
+    public class Box<T>
+    {
+        private List<T> data;
+
+        public int Count => this.data.Count;
+
+        public Box()
+        {
+            this.data = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            this.data.Add(item);
+        }
+
+        public T Remove()
+        {
+            var remove = this.data.Last();
+            this.data.RemoveAt(this.data.Count - 1);
+            return remove;
+        }
+    }
+}
