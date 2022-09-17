a681e5607f6b8d2aa9533e2603f1c40677a0457d
mases777 <mases@abv.bg>
2022-09-17 09:48:32 +0300

    M05-Zadacha03

---
+++ 2022-2023-M05/2022-M05-Templates/Zadacha03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var intScale = new Scale<int>(32, 41);
+            Console.WriteLine(intScale.GetHavier());
+
+            var strinScale = new Scale<string>("Ivan", "Peter");
+            Console.WriteLine(strinScale.GetHavier());
+        }
+    }
+}
