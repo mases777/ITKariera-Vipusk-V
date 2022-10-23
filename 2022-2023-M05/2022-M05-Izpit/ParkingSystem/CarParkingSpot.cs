1b757aceddbc2b124486eb6b3737b2ff6a39a1cd
mases777 <mases@abv.bg>
2022-10-23 12:34:50 +0300

    M05-Izpit

---
+++ 2022-2023-M05/2022-M05-Izpit/ParkingSystem/CarParkingSpot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class CarParkingSpot : ParkingSpot
+{
+    public CarParkingSpot(int id, bool occupied, double price) : base(id, occupied, "car", price)
+    {
+    }
+}
+
