7ebca960777d06819c7b4e8e30a790dfe5832404
mases777 <mases@abv.bg>
2022-10-30 13:10:57 +0200

    M05-Popravka

---
+++ 2022-2023-M05/2022-M05-Popravitelen-izpit/Zadacha01/JobOffer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+public abstract class JobOffer
+{
+    private string jobTitle;
+
+    public string JobTitle
+    {
+        get
+        {
+            return jobTitle;
+        }
+        set
+        {
+            if (value.Length < 3 || value.Length > 30)
+            {
+                throw new ArgumentException("JobTitle should be between 3 and 30 characters!");
+            }
+            jobTitle = value;
+        }
+    }
+
+    private string company;
+
+    public string Company
+    {
+        get
+        {
+            return company;
+        }
+        set
+        {
+            if (value.Length < 3 || value.Length > 30)
+            {
+                throw new ArgumentException("Company should be between 3 and 30 characters!");
+            }
+            company = value;
+        }
+    }
+
+    private double salary;
+
+    public double Salary
+    {
+        get
+        {
+            return salary;
+        }
+        set
+        {
+            if (salary < 0)
+            {
+                throw new ArgumentException("Salary should be 0 or positive!");
+            }
+            salary = value;
+        }
+    }
+
+    public JobOffer(string jobTitle, string company, double salary)
+    {
+        JobTitle = jobTitle;
+        Company = company;
+        Salary = salary;
+    }
+
+    public override string ToString()
+    {
+        return $"Job Title: {jobTitle}\n"
+             + $"Company: {company}\n"
+             + string.Format("Salary: {0:F2} BGN", salary);
+    }
+}
---
+++ 2022-2023-M05/2022-M05-Popravitelen-izpit/Zadacha01/RemoteJobOffer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class RemoteJobOffer : JobOffer
+{
+    private bool fullyRemote;
+
+    public RemoteJobOffer(string jobTitle, string company, double salary, bool fullyRemote) : base(jobTitle, company, salary)
+    {
+        JobTitle = jobTitle;
+        Company = company;
+        Salary = salary;
+        FullyRemote = fullyRemote;
+    }
+
+    public bool FullyRemote
+    {
+        get
+        {
+            return fullyRemote;
+        }
+        set
+        {
+            fullyRemote = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + "\n"
+             + $"Fully Remote: " + (fullyRemote ? "yes" : "no");
+    }
+}
