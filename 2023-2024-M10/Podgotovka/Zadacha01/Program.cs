5c45edba9efa96f0a69cf28b35122fd71a02770c
mases777 <mases@abv.bg>
2023-10-07 10:50:26 +0300

    M10-Zadacha01

---
+++ 2023-2024-M10/Podgotovka/Zadacha01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zadacha01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fufu = new List<string>();
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                for (char j = 'a'; j <= 'z'; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    string word = i.ToString() + j.ToString();
+                    fufu.Add(word);
+                }
+            }
+            var line = Console.ReadLine();
+            if (!fufu.Contains(line))
+            {
+                Console.WriteLine("No FuFu");
+            }
+            else
+            {
+                Console.WriteLine(fufu.IndexOf(line) + 1);
+            }
+        }
+    }
+}
